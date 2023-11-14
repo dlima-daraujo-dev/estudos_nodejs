4abfd6596647b73b58f3921750cdf7afd5ed71d3
dlima-daraujo-dev <dlima.daraujo.dev@gmail.com>
2023-11-14 18:23:26 -0300

    j bk

--- 004-Trabalhando-com-MongoDb/aprendendo.js
+++ 004-Trabalhando-com-MongoDb/aprendendo.js
@@ -1,3 +1,50 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost")
+// Config Mongoose
+    mongoose.Promise = global.Promise;
+    mongoose.connect("mongodb://localhost/teste").then(()=>{
+        console.log("MongoDB Conectado!!!")
+    }).catch((err)=>{
+        console.log("Erro ao se conectar!!! : "+ err)
+    })
+
+// Model - Usuários
+    // Definindo Model
+        const UserSchema = mongoose.Schema({
+            nome: {
+                type: String,
+                require: true
+            },
+            sobrenome: {
+                type: String,
+                requeri: true
+            },
+            email: {
+                type: String,
+                requeri: true
+            },
+            idade: {
+                type: Number,
+                requeri: true
+            },
+            pais: {
+                type: String
+            }
+        })
+
+    // Definindo a Collection
+        mongoose.model('Users', UserSchema)
+
+    // Add User na Collection Users
+        const Users = mongoose.model("Users")
+        new Users({
+            nome:"Jhennifer",
+            sobrenome:"Brandão",
+            email: "email@email.com",
+            idade: 27,
+            pais: "Brasil"
+        }).save().then(()=>{
+            console.log("Usuario Criado com sucesso!!")
+        }).catch((err)=>{
+            console.log("Houve um erro ao registrar o Usúario: "+err)
+        })
---
+++ 004-Trabalhando-com-MongoDb/db.js
@@ -0,0 +1,5 @@
+// Comando para ver as Collections do MongoDB
+    show Collection
+
+// Comando Usado para ver o que tem em uma determinada Collection aq no caso ela se chama users
+    db.users.find()
