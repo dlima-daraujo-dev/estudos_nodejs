09ca0cb05fb58d125b885ad208805e530c9d36ef
Maurício de Lima de Araújo <146596378+dlima-daraujo-dev@users.noreply.github.com>
2025-09-05 23:27:27 -0300

    Delete 002-http_protocolo-e-express directory

--- 002-http_protocolo-e-express/http.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");
-
-http.createServer(function(req, res){
-    res.end("Olá Server Online!");
-}).listen(8081);
-// Esse comando acima senve para criar um servidor web, a function tem doi atributos que é p "req" e "res". O comando "res.end("Olça Sever Online") mostra uma mensagem em localhost:8081
-
-console.log("Servidor Rodando!!")
