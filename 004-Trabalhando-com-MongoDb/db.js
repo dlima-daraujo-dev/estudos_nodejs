68f8210be0dceff6321e6338c7c4e4b822686f20
Maurício de Lima de Araújo <146596378+dlima-daraujo-dev@users.noreply.github.com>
2025-09-05 23:29:39 -0300

    Delete 004-Trabalhando-com-MongoDb directory

--- 004-Trabalhando-com-MongoDb/db.js
+++
@@ -1,5 +0,0 @@
-// Comando para ver as Collections do MongoDB
-    show Collection
-
-// Comando Usado para ver o que tem em uma determinada Collection aq no caso ela se chama users
-    db.users.find()
